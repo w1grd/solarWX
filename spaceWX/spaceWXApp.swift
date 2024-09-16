bae06bbfa00179e40517a79710b75a09cd2455c7
Perry Donham <perry@w1grd.radio>
2024-09-16 12:00:07 -0400

    Initial Commit

---
+++ spaceWX/spaceWXApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceWXApp.swift
+//  spaceWX
+//
+//  Created by Perry Donham on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct spaceWXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
